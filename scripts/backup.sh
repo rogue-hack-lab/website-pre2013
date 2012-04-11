92c73f193bac8abe32563c89a4802f9ed29e7f44
J. David Lowe <j.david.lowe@gmail.com>
2012-04-10 23:48:06 -0700

    just static pages for now, nothin' fancy...

--- scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# for backing up the mediawiki
-# copied from http://www.mediawiki.org/wiki/User:Flominator/Backup_MW
-# install in /var/lib/mediawiki
-FNAME=`date +%Y-%m-%d`
-mysqldump --database wikidb -u root -pthisisntthepassword  --add-drop-table -B > ${FNAME}.sql
-zip -r ./backup/${FNAME}.zip images/ ${FNAME}.sql LocalSettings.php extensions/
-rm ${FNAME}.sql
