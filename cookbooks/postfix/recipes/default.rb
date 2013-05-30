0853b7705327aefad09d0032f656b4c1a2f35ad8
J. David Lowe <j.david.lowe@gmail.com>
2013-05-29 19:00:05 -0700

    postfix configuration

--- cookbooks/postfix/recipes/default.rb
+++ cookbooks/postfix/recipes/default.rb
@@ -1,1 +1,30 @@
 package "postfix"
+
+cookbook_file "/etc/mailname" do
+  source "mailname"
+  mode "0644"
+  owner "root"
+  group "root"
+end
+
+cookbook_file "/etc/postfix/main.cf" do
+  source "main.cf"
+  mode "0644"
+  owner "root"
+  group "root"
+end
+
+cookbook_file "/etc/aliases" do
+  source "aliases"
+  mode "0644"
+  owner "root"
+  group "root"
+end
+
+script "restart postfix" do
+  interpreter "bash"
+  code <<END_CODE
+newaliases
+service postfix restart
+END_CODE
+end
