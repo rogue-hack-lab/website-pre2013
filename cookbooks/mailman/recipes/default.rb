d328822c4408c6d1cd52cfb647cbe9cd6c1cb2c4
J. David Lowe <j.david.lowe@gmail.com>
2013-05-29 20:04:29 -0700

    mailman basically working

--- cookbooks/mailman/recipes/default.rb
+++ cookbooks/mailman/recipes/default.rb
@@ -1,1 +1,15 @@
 package "mailman"
+
+cookbook_file "/etc/mailman/mm_cfg.py" do
+  source "mm_cfg.py"
+  mode "0644"
+  owner "root"
+  group "root"
+end
+
+script "restart mailman" do
+  interpreter "bash"
+  code <<END_CODE
+/etc/init.d/mailman force-reload
+END_CODE
+end
